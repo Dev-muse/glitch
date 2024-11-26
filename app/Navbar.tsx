09c3a137b695254f89b88b0bd0580aa52f06da70
Rahman <museabdirahman@gmail.com>
2024-11-26 20:31:18 +0000

    created nav component and setup prisma

---
+++ app/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "./assets/logo.svg";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+  const menu = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "issues" },
+  ];
+
+  return (
+    <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14 ">
+      <Link href="/">
+        <Image src={Logo} alt="Glitch logo" width={150} />
+      </Link>
+      <ul className="flex space-x-6">
+        {menu.map((route) => {
+          return (
+            <li
+              key={route.href}
+              className={classNames({
+                "text-orange-500": route.href == currentPath,
+                "text-zinc-400": route.href !== currentPath,
+                "hover:text-orange-500 text-lg transition-colors duration-300":
+                  true,
+              })}
+            >
+              <Link href={route.href}>{route.label}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Navbar from "./Navbar";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
