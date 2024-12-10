54956d191ea770a95e2bf6e80aa259fb069960ea
Rahman <museabdirahman@gmail.com>
2024-12-10 21:17:29 +0000

    created an api for deleting an issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -37,3 +37,22 @@ export async function PATCH(
     return NextResponse.json(updatedIssue, { status: 200 });
   }
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  } else {
+    const removedIssue = await prisma.issue.delete({
+      where: { id: issue.id },
+    });
+
+    return NextResponse.json(removedIssue, { status: 200 });
+  }
+}
