bf39086a3be4b4eaa752bab677ba192b4e3b1c78
Rahman <museabdirahman@gmail.com>
2024-12-10 20:56:52 +0000

    Implemented confirmation dialog for delete button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,34 @@
-import { Button } from "@radix-ui/themes";
-import React from "react";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button size={"2"} color="red">
-      Delete
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue?. This action cannot be
+          reversed.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
